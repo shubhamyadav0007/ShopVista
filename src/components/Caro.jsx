5edd350fa492101db9ae9c004b36d6fe99d7b4fa
Shubham Yadav <sy335740@gmail.com>
2024-01-28 17:58:10 +0530

    first commit

---
+++ src/components/Caro.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+export default function Caro() {
+    return (
+        <div>
+            <div
+                id="carouselExampleControls"
+                className="carousel slide"
+                data-bs-ride="carousel"
+            >
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="..." className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="..." className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="..." className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+    )
+}
